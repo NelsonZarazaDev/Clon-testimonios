fa765773ff843eb41b91a98a38f12af2e9719078
NelsonZaraza <nelsonmauricionavarrozaraza@gmail.com>
2023-12-27 17:14:53 -0500

    Clon de testimonios con react terminado

---
+++ src/component/Testimonio.js
@@ -0,0 +1,20 @@
+import React from "react";
+import '../css/Testimonio.css'; //El estilo se importa al componente que pertenece
+
+function Testimonio(props){
+  return(
+    <div className="contenedor-testimonio">
+      <img className="imagen-testimonio" 
+      src={require(`../img/testimonio-${props.imagen}.png`)} 
+      alt={`Foto de ${props.imagen}`}/>
+      <div className="contenedor-texto-testimonio">
+        <p className="nombre-testimonio"><strong>{props.nombre}</strong> en {props.pais}</p>
+        <p className="cargo-testimonio">{props.cargo} en <strong>{props.empresa}</strong></p>
+        <p className="texto-testimonio">"{props.testimonio}" </p>
+      </div>
+    </div>
+  )
+}
+
+//Exportamos testimonio para que pueda ser leido por otro archivo
+export default Testimonio; 
